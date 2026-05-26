767058e1f09a080a8db316315a7d5bee33b3a4b8
JuanPayes <00005923@uca.edu.sv>
2026-05-25 19:34:31 -0600

    Creando repositorio y mappers

---
+++ src/main/java/com/uca/pncsegundoparcialveterinaria/repository/ProductRepository.java
@@ -0,0 +1,6 @@
+package com.uca.pncsegundoparcialveterinaria.repository;
+
+import com.uca.pncsegundoparcialveterinaria.domain.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {}
--- src/main/java/com/uca/pncsegundoparcialveterinaria/utils/mappers/ProductMapper.java
+++ src/main/java/com/uca/pncsegundoparcialveterinaria/utils/mappers/ProductMapper.java
@@ -1,5 +1,65 @@
 package com.uca.pncsegundoparcialveterinaria.utils.mappers;
 
+import com.uca.pncsegundoparcialveterinaria.domain.dto.rquest.CreateProductRequest;
+import com.uca.pncsegundoparcialveterinaria.domain.dto.rquest.UpdateProductRequest;
+import com.uca.pncsegundoparcialveterinaria.domain.entities.Product;
+
 public class ProductMapper {
 
+    public static Product toEntity(CreateProductRequest request){
+        return Product.builder()
+                .name(request.name())
+                .description(request.description())
+                .price(request.price())
+                .stock(request.stock())
+                .available(request.available())
+                .category(request.category())
+                .expirationDate(request.expirationDate())
+                .requiredPrescription(request.requiredPrescription())
+                .supplier(request.supplier())
+                .build();
+    }
+
+    public static CreateProductRequest toDTO(Product product){
+        return CreateProductRequest.builder()
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .stock(product.getStock())
+                .available(product.getAvailable())
+                .category(product.getCategory())
+                .expirationDate(product.getExpirationDate())
+                .requiredPrescription(product.getRequiredPrescription())
+                .supplier(product.getSupplier())
+                .build();
+    }
+
+    public static UpdateProductRequest toUpdateEntity(UpdateProductRequest request){
+
+        return UpdateProductRequest.builder()
+                .name(request.name())
+                .description(request.description())
+                .price(request.price())
+                .stock(request.stock())
+                .available(request.available())
+                .category(request.category())
+                .expirationDate(request.expirationDate())
+                .requiredPrescription(request.requiredPrescription())
+                .supplier(request.supplier())
+                .build();
+    }
+
+    public static UpdateProductRequest toUpdateDTO(Product product){
+        return UpdateProductRequest.builder()
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .stock(product.getStock())
+                .available(product.getAvailable())
+                .category(product.getCategory())
+                .expirationDate(product.getExpirationDate())
+                .requiredPrescription(product.getRequiredPrescription())
+                .supplier(product.getSupplier())
+                .build();
+    }
 }
